3676c36c8ab09a8157d0964eb0e45cba2815680c
Simone <startae14@gmail.com>
2022-01-21 17:19:35 +0100

    02: common types in Typescript

---
+++ 02_types-in-typescript.js
@@ -0,0 +1,17 @@
+// boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = 'brown';
+var favouriteQuote = "I'm not old, I'm only ".concat(age);
+// Array
+var pets = ['cat', 'dog', 'pig'];
+var pets2 = ['lion', 'dragon', 'lizard'];
+// Object
+var wizard = {
+    a: 'John'
+};
+// null and undefined
+var meh = undefined;
+var noo = null;
---
+++ 02_types-in-typescript.ts
@@ -0,0 +1,22 @@
+// boolean
+let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = 'brown';
+let favouriteQuote: string = `I'm not old, I'm only ${age}`;
+
+// Array
+let pets: string[] = ['cat', 'dog', 'pig'];
+let pets2: Array<string> = ['lion', 'dragon', 'lizard'];
+
+// Object
+let wizard: object = {
+    a: 'John'
+};
+
+// null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
